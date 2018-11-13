9f016b4252adfb1896b6b030b272575997444a32
Cristi Scorta <cristi.scorta@yahoo.ro>
2018-11-13 19:31:45 +0200

    update

---
+++ .idea/src/For.java
@@ -0,0 +1,12 @@
+public class For {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+            for(int j=1;j<=10;j++){
+                System.out.print(i);
+                System.out.print('*');
+                System.out.print(j);
+                System.out.print('=');
+                System.out.println(i*j);
+            }
+    }
+}
